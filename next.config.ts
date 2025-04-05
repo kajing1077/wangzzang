644edfb3483abd7e0af6f82a22e2ee7faae8375b
kajing1077 <kajing1077@gmail.com>
2025-04-05 17:10:06 +0900

    Update Next.js configuration to set base path and enable unoptimized images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ import createMDX from "@next/mdx";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/wangzzang",
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 const withMDX = createMDX({
